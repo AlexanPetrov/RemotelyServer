f761e0d606356997daf85c8fb2d8643d5fa5e802
farhan haider <fhaider085@gmail.com>
2022-05-13 09:50:58 -0400

    fix: Jobs ordered by date + jobs posting to database

--- routes/jobs.js
+++ routes/jobs.js
@@ -4,17 +4,25 @@ import supabase from "../startup/db.js";
 const app = express();
 
 app.get("/", async (req, res) => {
-  let { data, error } = await await supabase.from("jobs").select("*");
-  console.log(data);
-  console.log(data.length);
+  /* Destructuring the data and error from the supabase query. */
+  let { data, error } = await await supabase
+    .from("jobs")
+    .select("*")
+    .order("created_at", { ascending: false });
 
+  /* This is a conditional statement that checks if there is an error in the query. If there is an
+  error, it will log the error and return a 500 status code. If there is no error, it will return
+  the data and a 200 status code. */
   if (error) {
+    console.log("Error");
     console.log(error);
+    res.status(500).json({ error });
+  } else {
+    res.status(200).json({ data });
   }
-
-  res.send("api is running");
 });
 
+/* This is a post request that is used to add a new job to the database. */
 app.post("/", async function (req, res) {
   const {
     company_name,
@@ -40,11 +48,13 @@ app.post("/", async function (req, res) {
     },
   ]);
 
-  console.log(data);
   if (error) {
     console.log(error);
+    res.status(500).json({ error });
+  } else {
+    console.log(data);
+    res.status(200).json({ data });
   }
-  res.send("posted done!");
 });
 
 export default app;
